c9bedb053921c2538be80cfed786618921ae3c21
NAH09 <maquantifyafrica@gmail.com>
2022-06-28 15:55:25 +0000

    Importation de bases de données Titanic

--- essay.py
+++ essay.py
@@ -1,6 +1,9 @@
+import imp
 import pandas as pd
 import seaborn as se
 import os
 import matplotlib as plt
-df = pd.read_csv('C:/Users/dell/Documents/draft/pokemon_data.csv')
-
+df = pd.read_csv('C:/Users/dell/train_and_test2.csv')
+df.head(10)
+type(df)
+df.columns
