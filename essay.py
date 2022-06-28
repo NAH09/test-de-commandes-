3e267920a7ea67d7178b3b34709fbbac5025cf12
NAH09 <maquantifyafrica@gmail.com>
2022-06-28 16:20:09 +0000

    Premiers plot

--- essay.py
+++ essay.py
@@ -2,8 +2,12 @@ import imp
 import pandas as pd
 import seaborn as se
 import os
-import matplotlib as plt
+import matplotlib.pyplot as plt
 df = pd.read_csv('C:/Users/dell/train_and_test2.csv')
 df.head(10)
 type(df)
 df.columns
+se.countplot(x="Embarked",data=df)
+plt.show()
+se.countplot(x="Embarked",hue="Sex",data=df)
+plt.show()
